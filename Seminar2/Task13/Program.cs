1f062cf9952f756f2b66e6bfaa58ac25fdddd60c
ArseniyZub <zubkov_000@list.ru>
2022-10-17 22:24:39 +0300

    Задача 13

---
+++ Seminar2/Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int value = int.Parse(Console.ReadLine()!);
+int result = 0;
+if (value/100 > 0)
+{
+    result = value%1000/100;
+    Console.WriteLine(result);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}

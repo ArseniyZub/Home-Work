7c7ff5ac92e98785c90bec78d598e7961207fedb
ArseniyZub <zubkov_000@list.ru>
2022-11-09 19:26:36 +0300

    Задача 29

---
+++ Seminar4/Task29/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] GetArray()
+{
+    int[] array = new int[8];
+    for(int i = 0; i < 8; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+Console.Write("Введите числа: ");
+Console.WriteLine(string.Join(",", GetArray()));
+
+
+
+
+
+
+
+
+

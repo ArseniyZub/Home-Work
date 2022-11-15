05d1b439678f37ab1dbf04084e6212e67d73a425
ArseniyZub <zubkov_000@list.ru>
2022-11-15 17:44:41 +0300

    Задача 41

---
+++ Seminar6/Task41/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] GetArrray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int Count(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите количество чисел M: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = GetArrray(size);
+
+int result = Count(array);
+Console.WriteLine($"{String.Join(", ", array)} -> {result}");

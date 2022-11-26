03efd8db57b6d687674c38a57f3dc7b905234a8e
ArseniyZub <zubkov_000@list.ru>
2022-11-26 19:25:15 +0300

    Задача 64

---
+++ Seminar9/Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+void IntervalNum(int num)
+{
+    if(num <= 0) return;
+    if (num >= 1)
+    {
+        Console.Write(num + ",");
+        IntervalNum(num - 1);
+    }
+}
+
+int num = int.Parse(Console.ReadLine()!);
+IntervalNum(num);
